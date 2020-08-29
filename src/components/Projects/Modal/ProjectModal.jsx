0343bb5ea95756e66d6d9cb51c9acc462ec95daa
vatsalkgor <vatsalkgor@gmail.com>
2020-08-30 02:34:31 +1200

    project modal added and some improvements in project tile

---
+++ src/components/Projects/Modal/ProjectModal.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Modal, Card } from "react-bootstrap";
+import { IoLogoGithub } from "react-icons/io";
+import GridGenerator from "../GridGenerator/GridGenerator";
+
+export default function ProjectModal({ show, handleClose, project }) {
+    console.log(project);
+    return (
+        <Modal show={show} onHide={handleClose} centered size="lg" scrollable>
+            <Modal.Header closeButton>
+                <Modal.Title>{project.title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="long-description">
+                    <div className="subheading">
+                        <b>Overview</b>
+                    </div>
+                    <div>
+                        {project.long_description.overview.map((entry, i) => (
+                            <p key={i}>{entry}</p>
+                        ))}
+                    </div>
+                </div>
+                <div className="long-description">
+                    <div className="subheading">
+                        <b>Requirements</b>
+                    </div>
+                    <div>
+                        {project.long_description.requirements.map(
+                            (entry, i) => (
+                                <p key={i}>{entry}</p>
+                            )
+                        )}
+                    </div>
+                </div>
+                <div className="long-description">
+                    <div className="subheading">
+                        <b>Solution</b>
+                    </div>
+                    <div>
+                        {project.long_description.solution.map((entry, i) => (
+                            <p key={i}>{entry}</p>
+                        ))}
+                    </div>
+                </div>
+                <div className="long-description">
+                    <div className="subheading">
+                        <b>Result</b>
+                    </div>
+                    <div>
+                        {project.long_description.result.map((entry, i) => (
+                            <p key={i}>{entry}</p>
+                        ))}
+                    </div>
+                </div>
+                <div className="subheading">
+                    <b>Technologies</b>
+                </div>
+                <div>
+                    <ul>
+                        {project.tech.map((tech, i) => (
+                            <li key={i}>{tech}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="subheading">
+                    <b>Timeline</b>
+                </div>
+                <div>
+                    <b>Duration: </b>
+                    {project.timeline.duration} Month(s)
+                    <span>&nbsp;&nbsp;</span>
+                    <b>Months: </b>
+                    {project.timeline.months}
+                </div>
+                <div>
+                    {project.images && (
+                        <>
+                            <div className="subheading">
+                                <b>Work sample</b>
+                            </div>
+                            <GridGenerator cols={3}>
+                                {project.images.map((link, i) => (
+                                    <Card style={{ margin: "16px" }} key={i}>
+                                        <Card.Img src={link} />
+                                    </Card>
+                                ))}
+                            </GridGenerator>
+                        </>
+                    )}
+                </div>
+            </Modal.Body>
+            {project.link && (
+                <Modal.Footer style={{ padding: "0px" }}>
+                    <a
+                        className="float-right"
+                        target="_blank"
+                        href={project.link}
+                        rel="noopener noreferrer"
+                    >
+                        <IoLogoGithub size="2em"></IoLogoGithub>
+                    </a>
+                </Modal.Footer>
+            )}
+        </Modal>
+    );
+}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,9 +6,16 @@ import GridGenerator from "./GridGenerator/GridGenerator";
 import Tile from "./Tile/Tile";
 import { getProjects } from "./Firebase";
 import { useState } from "react";
+import ProjectModal from "./Modal/ProjectModal";
 
 export default function Projects() {
     const [projects, setProjects] = useState();
+    const [modalProps, setModalProps] = useState({
+        show: false,
+        project: null,
+    });
+    const handleClose = () => setModalProps({ show: false });
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
         async function getProjectsData() {
@@ -38,26 +45,24 @@ export default function Projects() {
                         md={{ span: 6, offset: 3 }}
                         sm={12}
                     >
-                        <div
-                            className="text-center"
-                            style={{ textAlign: "justify" }}
-                        >
+                        <div className="text-justify">
                             I like the satisfaction of creating something from
                             scratch, giving birth to a new Application with a
                             purpose that slowly acquires it's soul. The Back-End
                             Development excites me while the Front-End and
                             Design aspects of an Application brings out all the
-                            well-thought work that has been done. I continuously
-                            learn new things, new Libraries, new Languages and
-                            new Frameworks through Documentations or Articles. I
-                            stay up-to-date with the quickly-evolving area of
-                            Software Engineering. Some of the topics that I have
-                            studied on are Restfull APIs, Single Page Cross
-                            Platform Desktop &amp; Mobile Applications Databases
-                            ( MySQL, MongoDB, Postgres, Firestore), different
-                            languages (NodeJS, React, Angular, PHP, Java,
-                            TypeScript, Python and Flask) while also planning to
-                            look into more aspects (AI, Python, Django, PWAs).
+                            well-thought work that has been put into it. I
+                            continuously learn new things, new Libraries, new
+                            Languages and new Frameworks through Documentations
+                            or Articles. I stay up-to-date with the
+                            quickly-evolving area of Software Engineering. Some
+                            of the topics that I have studied on are Restfull
+                            APIs, Single Page Cross Platform Desktop &amp;
+                            Mobile Applications Databases ( MySQL, MongoDB,
+                            Postgres, Firestore), different languages (NodeJS,
+                            React, Angular, PHP, Java, TypeScript, Python and
+                            Flask) while also planning to look into more aspects
+                            (AI, Python, Django, PWAs).
                         </div>
                     </Col>
                 </Row>
@@ -67,11 +72,22 @@ export default function Projects() {
                     {projects && (
                         <GridGenerator cols={3}>
                             {projects.map((project, i) => (
-                                <Tile key={i} project={project} />
+                                <Tile
+                                    setModalProps={setModalProps}
+                                    key={i}
+                                    project={project}
+                                />
                             ))}
                         </GridGenerator>
                     )}
                 </div>
+                {modalProps.show && modalProps.project && (
+                    <ProjectModal
+                        show={modalProps.show}
+                        project={modalProps.project}
+                        handleClose={handleClose}
+                    />
+                )}
             </Container>
         </div>
     );
