232dc73918088bc54aace7ae1030c8ae1103c010
vatsalkgor <vatsalkgor@gmail.com>
2020-09-06 18:28:27 +1200

    add countdown to blogs

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "../Navigation";
 import { Container, Row, Col } from "react-bootstrap";
+import Countdown from "react-countdown";
 import "./Blogs.css";
 
 export default function Blogs() {
@@ -14,10 +15,35 @@ export default function Blogs() {
                         sm={12}
                         className="d-flex justify-content-center"
                     >
-                        <div className="heading">Blogs Coming Soon!</div>
+                        <div className="text-center">
+                            <div className="heading">Blogs Coming Soon!</div>
+                            <div className="subheading">
+                                1<sup>st</sup> blog releasing on 12<sup>th</sup>{" "}
+                                Sept 2020.
+                            </div>
+                            <div>
+                                <Countdown
+                                    date={1599912000000}
+                                    renderer={countdownRenderer}
+                                ></Countdown>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
         </div>
     );
 }
+
+const countdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+        return <div>Here you go!</div>;
+    } else {
+        return (
+            <span>
+                {days} Days {hours} Hours {minutes} Minutes {seconds} Seconds
+                Remaining!
+            </span>
+        );
+    }
+};
